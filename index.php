2808f8079521c7000c679e30d6e90b51402bd84d
Matheus <matheuspq79@outlook.com.br>
2018-07-03 21:49:21 -0300

    Admin Categorias

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use Hcode\Model\Category;
 $app = new Slim(); //Rotas
 
 $app->config('debug', true);
@@ -31,6 +32,7 @@ $app->get('/admin', function() {
 
 $app->get('/admin/login', function() {
 	
+	//Header e Footer do Pageadmin é toda a estrutura do AdminLTE em si.
 	$page = new PageAdmin([
 		"header" => false,
 		"footer" => false
@@ -213,6 +215,92 @@ $app->post("/admin/forgot/reset", function(){
 
 	$page->setTpl("forgot-reset-success");
 	
+});
+
+$app->get("/admin/categories", function(){
+	
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories' => $categories
+	]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+	
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+	
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+	
+	User::verifyLogin();
+	
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+	
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		'category' => $category->getValues() //ATENÇÃO: usar apóstrofo
+	]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+	
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST); //Inputs mesmo nome do banco
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
 });
 
 $app->run(); //Roda tudo!
