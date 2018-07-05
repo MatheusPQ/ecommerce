9b504cbf06d2c6366d6d97233ab2ed2f71a8282c
Matheus <matheuspq79@outlook.com.br>
2018-07-04 22:27:47 -0300

    Site produtos

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 $app = new Slim(); //Rotas
 
+require_once("functions.php");
 require_once("site.php");
 require_once("admin.php");
 require_once("admin-users.php");
--- site.php
+++ site.php
@@ -1,13 +1,17 @@
 <?php
 
-
 use \Hcode\Page;
+use \Hcode\Model\Product;
 
 $app->get('/', function() {
+
+	$products = Product::listAll();
 	
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		'products' => Product::checkList($products)
+	]);
 
 });//Quando chegar nesta linha, será chamado o desctruct do Page (que incluirá o Destruct)
 
