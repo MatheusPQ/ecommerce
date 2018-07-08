63d7f00b73057f423d9cbf1e959b4489ad0e5265
Matheus <matheuspq79@outlook.com.br>
2018-07-08 12:12:25 -0300

    Categories vs Products

--- site.php
+++ site.php
@@ -2,6 +2,7 @@
 
 use \Hcode\Page;
 use \Hcode\Model\Product;
+use \Hcode\Model\Category;
 
 $app->get('/', function() {
 
@@ -14,6 +15,21 @@ $app->get('/', function() {
 	]);
 
 });//Quando chegar nesta linha, será chamado o desctruct do Page (que incluirá o Destruct)
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category' => $category->getValues(), //ATENÇÃO: usar apóstrofo
+		'products' => Product::checkList($category->getProducts())
+	]);
+
+});
 
 
 ?>
