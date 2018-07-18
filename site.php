b7bb9bd7e40704b7106ca939d9f031cdb11bd255
Matheus <matheuspq79@outlook.com.br>
2018-07-17 22:16:08 -0300

    Carrinho produtos

--- site.php
+++ site.php
@@ -66,7 +66,55 @@ $app->get("/cart", function(){
 
 	$page = new Page();
 
-	$page->setTpl("cart");
+	$page->setTpl("cart", [
+		'cart' => $cart->getValues(),
+		'products' => $cart->getProducts(),
+	]);
+});
+
+$app->get("/cart/:idproduct/add", function($idproduct){
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession(); //Recupera o carrinho da sessão.. ou cria um carrinho novo.
+
+	//Campo qtd da tela product-detail.html
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+
+	for ($i=0; $i < $qtd ; $i++) { 
+		$cart->addProduct($product);
+	}
+
+	header("Location: /cart");
+	exit;
+});
+
+//Remove apenas 1 produto do carrinho
+$app->get("/cart/:idproduct/minus", function($idproduct){
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession(); //Recupera o carrinho da sessão.. ou cria um carrinho novo.
+
+	$cart->removeProduct($product);
+
+	header("Location: /cart");
+	exit;
+});
+
+$app->get("/cart/:idproduct/remove", function($idproduct){
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession(); //Recupera o carrinho da sessão.. ou cria um carrinho novo.
+
+	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
 });
 
 ?>
